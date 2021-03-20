9d5863020b6cfdf94dab3f3dc6405416e0d3b5aa
Shivashankar <shivashankar@myelinfoundry.com>
2021-03-20 11:05:53 +0530

    Downloading and displaying previous day data if curret day data is not available when user queries for a ticker record

---
+++ marketinfo/bhavcopyapp/data_helper.py
@@ -0,0 +1,132 @@
+from django.http import HttpResponse
+from django.conf import settings
+
+from datetime import datetime
+from datetime import timedelta
+import requests
+import tempfile
+import zipfile
+import os
+import pytz
+import redis
+import csv
+import json
+
+
+
+class StorageHelper:
+
+    def __init__(self):
+
+        self.bse_link = "https://www.bseindia.com/download/BhavCopy/Equity/"
+        self.folder_dir = "./bhavcopyapp/files/"
+
+        self.redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
+                                        port=settings.REDIS_PORT, db=0)
+        
+
+        IST = pytz.timezone("Asia/Kolkata")
+
+        self.current_date = datetime.now(IST)
+        self.previous_date = self.current_date - timedelta(days=1)
+
+        self.current_day = self.current_date.strftime("%d")
+        self.current_month = self.current_date.strftime("%m")
+        self.current_year = self.current_date.strftime("%y")
+
+        self.previous_day = self.previous_date.strftime("%d")
+        self.previous_month = self.previous_date.strftime("%m")
+        self.previous_year = self.previous_date.strftime("%y")
+
+
+
+        self.current_zip_file_name = f"EQ{self.current_day}{self.current_month}{self.current_year}_CSV.ZIP"
+
+        self.current_csv_file_name = ((self.current_zip_file_name.split(".")[0]).split("_")[0])+".CSV"
+
+        self.previous_zip_file_name = f"EQ{self.previous_day}{self.previous_month}{self.previous_year}_CSV.ZIP"
+
+        self.previous_csv_file_name = ((self.previous_zip_file_name.split(".")[0]).split("_")[0])+".CSV"
+
+    def save_to_redis(self,csv_file_name):
+        
+        with open(self.folder_dir+csv_file_name,"r") as csv_file:
+            csv_reader =  csv.reader(csv_file) 
+            headers = next(csv_reader)
+
+            for row in csv_reader:
+                self.redis_instance.set(row[1].strip(),json.dumps({"code":row[0],
+                                                    "open":row[4],
+                                                    "high":row[5],
+                                                    "low":row[6],
+                                                    "close":row[7]}))
+            
+            self.redis_instance.set("previous_data","True")
+    
+    def get_records(self,record_name):
+        if self.redis_instance.get("current_data") is None:
+
+            # fetch previous day record and send the response back
+
+            if self.redis_instance.get("previous_data") is None:
+                self.download_data()
+                if self.redis_instance.get("previous_data") is None:
+                    return (None,False)
+
+                else:
+                    return (self.redis_instance.get(record_name),False)
+            
+            else:
+                print("success")
+                return (self.redis_instance.get(record_name),False)
+         
+        else:
+            print(record_name)
+            return (self.redis_instance.get(record_name),True)
+
+
+
+
+# Create your views here.
+
+    def download_data(self):
+
+        
+        print("Downloading previous data")
+        
+
+        hdr = {'User-Agent': 'Mozilla/5.0'}
+
+        response = requests.get(self.bse_link+self.previous_zip_file_name,stream=True,headers=hdr)
+        
+
+        if response.status_code != requests.codes.ok :
+            # if previous data is not available return no data
+            self.redis_instance.set("previous_data","False")
+            return 
+  
+        # print(os.listdir("./bhavcopyapp/"))
+
+        with open(self.folder_dir+self.previous_zip_file_name,"wb") as csv_file:
+            # lf = tempfile.NamedTemporaryFile()
+            for block in response.iter_content(1024*8):
+                if not block:
+                    break
+
+                csv_file.write(block)
+
+            csv_file.close()
+
+        with zipfile.ZipFile(self.folder_dir+self.previous_zip_file_name, 'r') as zip_ref:
+            zip_ref.extractall(self.folder_dir)
+        
+        os.remove(self.folder_dir+self.previous_zip_file_name)
+
+        print("Previous Data downloaded")
+        self.save_to_redis(self.previous_csv_file_name)
+
+
+
+        
+            
+
--- marketinfo/bhavcopyapp/views.py
+++ marketinfo/bhavcopyapp/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.http.response import JsonResponse
 from datetime import datetime
 
+from bhavcopyapp import data_helper
 
 import requests
 import tempfile
@@ -14,80 +15,76 @@ import redis
 import csv
 import json
 
-bse_link = "https://www.bseindia.com/download/BhavCopy/Equity/"
-folder_dir = "./bhavcopyapp/files/"
+s = data_helper.StorageHelper()
 
-redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
-                                  port=settings.REDIS_PORT, db=0)
-
-# Create your views here.
 
 def get_data(request):
     # print(redis_instance.get("HDFC        "))
 
-    return JsonResponse(json.loads(redis_instance.get("HDFC")))
+
+    ticker_name = request.GET["name"].strip()
+    ticker_record = s.get_records(ticker_name)
+
+    
+
+    if ticker_record[0]!=None:
+        response = json.loads(ticker_record[0])
+        response["current_data"]=ticker_record[1]
+        return JsonResponse(response)
+    else:
+        return JsonResponse({"data":ticker_record[0],"previous_data":False,"current_data":False,"code":404})
+
+
+
+    # return JsonResponse(json.loads(redis_instance.get("HDFC")))
 
 def download_data(request):
+    pass
 
-    IST = pytz.timezone("Asia/Kolkata")
-    current_date = datetime.now(IST)
+    # IST = pytz.timezone("Asia/Kolkata")
+    # current_date = datetime.now(IST)
 
-    day = current_date.strftime("%d")
-    month = current_date.strftime("%m")
-    year = current_date.strftime("%y")
+    # day = current_date.strftime("%d")
+    # month = current_date.strftime("%m")
+    # year = current_date.strftime("%y")
 
-    zip_file_name = f"EQ{day}{month}{year}_CSV.ZIP"
+    # zip_file_name = f"EQ{day}{month}{year}_CSV.ZIP"
     
-    zip_file_name="EQ190321_CSV.ZIP"
+    # zip_file_name="EQ190321_CSV.ZIP"
 
-    csv_file_name = ((zip_file_name.split(".")[0]).split("_")[0])+".CSV"
+    # csv_file_name = ((zip_file_name.split(".")[0]).split("_")[0])+".CSV"
 
-    hdr = {'User-Agent': 'Mozilla/5.0'}
+    # hdr = {'User-Agent': 'Mozilla/5.0'}
 
-    response = requests.get(bse_link+zip_file_name,stream=True,headers=hdr)
-    
-
-    if response.status_code != requests.codes.ok :
-        return HttpResponse("Error downloading file")
-    
-    
-    # print(os.listdir("./bhavcopyapp/"))
-
-    with open(folder_dir+zip_file_name,"wb") as csv_file:
-        # lf = tempfile.NamedTemporaryFile()
-        for block in response.iter_content(1024*8):
-            if not block:
-                break
-
-            csv_file.write(block)
-
-        csv_file.close()
-
-    with zipfile.ZipFile(folder_dir+zip_file_name, 'r') as zip_ref:
-        zip_ref.extractall(folder_dir)
-    
-    os.remove(folder_dir+zip_file_name)
-
-    save_to_redis(csv_file_name)
-
-    return HttpResponse("data downloaded")
+    # response = requests.get(bse_link+zip_file_name,stream=True,headers=hdr)
     
 
-def save_to_redis(csv_file_name):
+    # if response.status_code != requests.codes.ok :
+    #     return HttpResponse("Error downloading file")
     
-    with open(folder_dir+csv_file_name,"r") as csv_file:
-        csv_reader =  csv.reader(csv_file) 
-        headers = next(csv_reader)
+    
+    # # print(os.listdir("./bhavcopyapp/"))
 
-        for row in csv_reader:
-            redis_instance.set(row[1].strip(),json.dumps({"code":row[0],
-                                                  "open":row[4],
-                                                  "high":row[5],
-                                                  "low":row[6],
-                                                  "close":row[7]}))
+    # with open(folder_dir+zip_file_name,"wb") as csv_file:
+    #     # lf = tempfile.NamedTemporaryFile()
+    #     for block in response.iter_content(1024*8):
+    #         if not block:
+    #             break
 
-    print(redis_instance.get("IFCI310312A "))
-        
+    #         csv_file.write(block)
+
+    #     csv_file.close()
+
+    # with zipfile.ZipFile(folder_dir+zip_file_name, 'r') as zip_ref:
+    #     zip_ref.extractall(folder_dir)
+    
+    # os.remove(folder_dir+zip_file_name)
+
+    # save_to_redis(csv_file_name)
+
+    # return HttpResponse("data downloaded")
+    
+
 
 
 
