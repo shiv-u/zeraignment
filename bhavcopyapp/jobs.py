5ef1a21cf01ba37e12123b8cfb1ab676bf7017df
Shivashankar <shivashankar@myelinfoundry.com>
2021-03-23 01:50:33 +0530

    Added a job to reset data at the end of the day - made it a cron job

--- bhavcopyapp/jobs.py
+++ bhavcopyapp/jobs.py
@@ -9,6 +9,6 @@ def start():
     data_resetter  = reset_job.DataResetter()
     scheduler = BackgroundScheduler()
     scheduler.add_job(download_manager.download_data, 'cron',hour=18,timezone="Asia/Kolkata",id="download_job")
-    # scheduler.add_job(data_resetter.reset, 'cron',hour=23,minute=59,second=59,timezone="Asia/Kolkata",id="reset_job")
-    scheduler.add_job(data_resetter.reset, 'interval',seconds=30,timezone="Asia/Kolkata",id="reset_job")
+    scheduler.add_job(data_resetter.reset, 'cron',hour=23,minute=59,second=59,timezone="Asia/Kolkata",id="reset_job")
+    # scheduler.add_job(data_resetter.reset, 'interval',seconds=30,timezone="Asia/Kolkata",id="reset_job")
     scheduler.start()
