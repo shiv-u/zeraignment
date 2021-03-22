680311975715ec49bcdfd430f75e470a3aa573c3
Shivashankar <shivashankar@myelinfoundry.com>
2021-03-22 21:05:05 +0530

    Removed unused views from app's urls.py

--- bhavcopyapp/urls.py
+++ bhavcopyapp/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="index"),
-    path('download',views.download_data,name="download_data"),
-    path("get_data",views.get_data,name="get_data"),
-    path("equity",views.servefiles,name="serve_files")
+    path("get_data",views.get_data,name="get_data")
 
 ]
